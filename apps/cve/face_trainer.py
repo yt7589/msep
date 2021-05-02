fc2052b17ed0fc6e5c1836856c8e7e22b230e827
yantao <yt7589@qq.com>
2021-05-02 11:47:20 +0800

    save train image from video stream

--- apps/cve/face_trainer.py
+++ apps/cve/face_trainer.py
@@ -1,5 +1,6 @@
 #
 import cv2
+from PIL import Image
 import face_recognition
 
 class FaceTrainer(object):
@@ -25,9 +26,24 @@ class FaceTrainer(object):
                 bottom_pos = bottom_pos*4
                 left_pos = left_pos*4
                 # enlarge the cripping box
+                left_pos -= int((right_pos - left_pos)*0.25)
+                if left_pos<0:
+                    left_pos = 0
+                top_pos -= int((bottom_pos - top_pos) * 0.6)
+                if top_pos < 0:
+                    top_pos = 0
+                right_pos += int((right_pos - left_pos) * 0.3)
+                if right_pos > frame.shape[1]:
+                    right_pos = frame.shape[1]
+                bottom_pos += int((bottom_pos - top_pos) * 0.2)
+                if bottom_pos > frame.shape[0]:
+                    bottom_pos = frame.shape[0]
                 #printing the location of current face
                 print('Found face {} at top:{},right:{},bottom:{},left:{}'.format(index+1,top_pos,right_pos,bottom_pos,left_pos))
                 current_face_image = frame[top_pos:bottom_pos,left_pos:right_pos]
+                if cv2.waitKey(1) & 0xFF == ord('a'):
+                    im = Image.fromarray(current_face_image)
+                    im.save("yt2.jpg")
                 #draw rectangle around the face detected
                 cv2.rectangle(frame,(left_pos,top_pos),(right_pos,bottom_pos),(0,0,255),2)
 
